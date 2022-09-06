f0282d1e422aea003df5b56e0aabfc4bee440db9
Akbar Bobomurodov <a.bobomurodov@yt.uz>
2022-09-06 06:54:40 +0500

    first commit

--- src/createRangeDoSomething.ts
+++
@@ -1,9 +0,0 @@
-export function createRangeAndDoSomething<T extends (i: number) => void>(
-  startIndex: number,
-  endIndex: number,
-  cb: T
-) {
-  for (let i = startIndex; i <= endIndex; i++) {
-    cb(i);
-  }
-}
